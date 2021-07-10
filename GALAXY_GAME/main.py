97580276abbfd94ca66b2c9acd3a7c68559f3c7e
KaliW <kaliw@kali>
2021-07-09 19:06:13 -0500

    Aprendiendo Botones, Layouts y Kivy files

---
+++ GALAXY_GAME/main.py
@@ -0,0 +1,14 @@
+from kivy.app import App
+from kivy.uix.widget import Widget
+from kivy.uix.boxlayout import BoxLayout
+from kivy.uix.button import Button
+
+class BoxLayoutExample(BoxLayout):
+    pass
+
+class MainWidget(Widget):  #Main interface
+    pass
+
+class TheLabApp(App):
+    pass
+TheLabApp().run()
