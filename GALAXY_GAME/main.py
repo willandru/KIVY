04dd23e1f7f9d65776fd602e77508890ead81078
KaliW <kaliw@kali>
2021-07-10 15:53:51 -0500

    Aprendiendo LAYOUTS: Grid,Anchor,Box Layouts

--- GALAXY_GAME/main.py
+++ GALAXY_GAME/main.py
@@ -2,6 +2,14 @@ from kivy.app import App
 from kivy.uix.widget import Widget
 from kivy.uix.boxlayout import BoxLayout
 from kivy.uix.button import Button
+from kivy.uix.anchorlayout import AnchorLayout
+from kivy.uix.gridlayout import GridLayout
+
+class GridLayoutExample(GridLayout):
+    pass
+
+class AnchorLayoutExample(AnchorLayout):
+    pass
 
 class BoxLayoutExample(BoxLayout):
     pass
