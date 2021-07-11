3c493ae1ddf63da90387563896c0b4d0d6972149
KaliW <kaliw@kali>
2021-07-10 22:44:02 -0500

    Aprendiendo:LAYOUTS:StackLayout, PageLayout and ScrollView

--- GALAXY_GAME/main.py
+++ GALAXY_GAME/main.py
@@ -4,6 +4,16 @@ from kivy.uix.boxlayout import BoxLayout
 from kivy.uix.button import Button
 from kivy.uix.anchorlayout import AnchorLayout
 from kivy.uix.gridlayout import GridLayout
+from kivy.uix.stacklayout import StackLayout
+from kivy.metrics import dp
+
+class StackLayoutExample(StackLayout):
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
+        self.orientation= "rl-tb"
+        for i in range(0,40):
+            b= Button(text=str(i+1),size_hint= (None, None), size=(dp(100), dp(100)))
+            self.add_widget(b)
 
 class GridLayoutExample(GridLayout):
     pass
